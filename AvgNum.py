5f2227bc4d3cb4f4c72cbc236ad08ed69b26335d
MolarPort850 <aaroncoddington97@gmail.com>
2021-01-18 13:22:37 -0600

    first commit of file and readme

---
+++ AvgNum.py
@@ -0,0 +1,12 @@
+def AverageNumber():
+    num_sum = 0
+    how_many_nums = int(input("How many numbers do you want to input?:\n"))
+    for number in range(how_many_nums):
+        current_num = int(input('what is the number?:\n'))
+        num_sum = num_sum + current_num
+        number += 1
+
+    print(f'your average is {num_sum/how_many_nums}')
+
+
+AverageNumber()
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Aaron's Average Code
+
+## This is a program that averages numbers
+
+This program uses a function to average numbers 
+=====================================================
